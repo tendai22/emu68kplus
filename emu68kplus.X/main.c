ced998c3d65ea5c4bdb524cc83ed6366801044d1
Norihiro Kumagai <tendai22plus@gmail.com>
2023-02-12 20:02:19 +0900

    firmware completed. upload commands, single-step.

--- emu68kplus.X/main.c
+++ emu68kplus.X/main.c
@@ -64,8 +64,10 @@
 #include <xc.h>
 #include <stdio.h>
 #include "param.h"
+#include "xprintf.h"
 
-#define M68k8_CLK 6000000UL // MC68008 clock frequency(Max 16MHz)
+//#define M68k8_CLK 6000000UL // MC68008 clock frequency(Max 16MHz)
+#define M68k8_CLK 2000000UL // MC68008 clock frequency(Max 16MHz)
 
 #define _XTAL_FREQ 64000000UL
 
@@ -80,7 +82,7 @@ union {
 
 
 // UART3 Transmit
-void putch(char c) {
+void putch(int c) {
     while(!U3TXIF); // Wait or Tx interrupt flag set
     U3TXB = c; // Write data
 }
@@ -91,6 +93,148 @@ char getch(void) {
     return U3RXB; // Read data
 }
 
+void HALT_on(void)
+{
+    TRISA5 = 0;
+    LATA5 = 0;
+}
+
+void HALT_off(void)
+{
+    TRISA5 = 1; // RESET is Open-Drain and pulled-up, so
+                // only do it input-mode is necessary
+}
+
+void RESET_on(void)
+{
+    TRISE0 = 0; // output
+    LATE0 = 0;
+}
+
+void RESET_off(void)
+{
+//    TRISE0 = 1; // set as input
+    LATE0 = 1;
+}
+
+void monitor(void)
+{
+    static int count = 0;
+    int halt_flag;
+    xprintf("%04X %02X %c%c%c %c\n", ab.w, PORTC, 
+        ((PORTA&4) ? 'R' : 'W'),
+        ((PORTA&(1<<5)) ? '-' : 'H'),
+        ((PORTE&(1<<0)) ? '-' : 'R'),
+        ((PORTE&(1<<1)) ? '-' : 'I'));
+                
+    if (count < 20) count++;
+    else getch();
+}
+
+static int uc = -1;
+int getchr(void)
+{
+    static int count = 0;
+    int c;
+    if (uc >= 0) {
+        c = uc;
+        uc = -1;
+        return c;
+    }
+    while ((c = getch()) == '.' && count++ < 2);
+    if (c == '.') {
+        count = 0;
+        return -1;
+    }
+    count = 0;
+    return c;
+}
+
+void ungetchr(int c)
+{
+    uc = c;
+}
+
+int is_hex(char c)
+{
+    if ('0' <= c && c <= '9')
+        return !0;
+    c &= ~0x20;     // capitalize
+    return ('A' <= c && c <= 'F');
+}
+
+int to_hex(char c)
+{
+    //xprintf("{%c}", c);
+    if ('0' <= c && c <= '9')
+        return c - '0';
+    c &= ~0x20;
+    if ('A' <= c && c <= 'F')
+        return c - 'A' + 10;
+    return -1;
+}
+
+typedef unsigned short addr_t;
+
+void manualboot(void)
+{
+    int c, d, n, count;
+    addr_t addr = 0, max = 0, min = RAM_TOP + RAM_SIZE;
+    int addr_flag = 0;
+    
+    while (1) {
+        while ((c = getchr()) == ' ' || c == '\t' || c == '\n' || c == '\r')
+            ;   // skip white spaces
+        if (c == -1)
+            break;
+        if (c == '!' && min < max) {
+            xprintf("\n");
+            // dump memory
+            addr_t start, end;
+            start = min & 0xfff0;
+            end = max;
+            while (start < end) {
+                if ((start & 0xf) == 0) {
+                    xprintf("%04X ", start);  
+                }
+                xprintf("%04X ", ((((unsigned short)ram[start])<<8)|ram[start+1]));
+                if ((start & 0xf) == 0xe) {
+                    xprintf("\n");
+                }
+                start += 2;                
+            }
+            continue;
+        }
+        addr_flag = (c == '=');
+        //xprintf("[%c]", c);
+        if (!addr_flag)
+            ungetchr(c);
+        // read one hex value
+        n = 0;
+        while ((d = to_hex(c = getchr())) >= 0) {
+            n *= 16; n += d;
+            //xprintf("(%x,%x)", n, d);
+        }
+        if (c < 0)
+            break;
+        if (d < 0) {
+            if (addr_flag) {  // set address
+                addr = n;
+            } else {
+                if (RAM_TOP <= addr && addr < (RAM_TOP + RAM_SIZE)) {
+                    //xprintf("[%04X] = %02X%02X\n", addr, ((n>>8)&0xff), (n & 0xff));
+                    ram[addr++] = ((n>>8) & 0xff);
+                    ram[addr++] = (n & 0xff);
+                    if (max < addr)
+                        max = addr;
+                    if (addr - 2 < min)
+                        min = addr - 2;
+                }
+            }
+            continue;
+        }
+    }
+}
 
 #define db_setin() (TRISC = 0xff)
 #define db_setout() (TRISC = 0x00)
@@ -100,6 +244,8 @@ void main(void) {
     // System initialize
     OSCFRQ = 0x08; // 64MHz internal OSC
 
+    // xprintf initialize
+    xdev_out(putch);
     // Address bus A15-A8 pin
     ANSELD = 0x00; // Disable analog function
     WPUD = 0xff; // Week pull up
@@ -119,6 +265,11 @@ void main(void) {
     ANSELE0 = 0; // Disable analog function
     LATE0 = 0; // RESET assert
     TRISE0 = 0; // Set as output
+
+    // HALT output pin
+    ANSELA5 = 0; // Disable analog function
+    LATA5 = 0; // HALT assert
+    TRISA5 = 0; // Set as output
 
     // IPL1 output pin
     ANSELE1 = 0; // Disable analog function
@@ -150,11 +301,6 @@ void main(void) {
     LATA4 = 1; // DTACK negate
     TRISA4 = 0; // Set as output
 
-    // HALT output pin
-    ANSELA5 = 0; // Disable analog function
-    LATA5 = 0; // HALT assert
-    TRISA5 = 0; // Set as output
-
     // Z80 clock(RA3) by NCO FDC mode
     RA3PPS = 0x3f; // RA3 asign NCO1
     ANSELA3 = 0; // Disable analog function
@@ -185,18 +331,21 @@ void main(void) {
     RA6PPS = 0x26;  //RA6->UART3:TX3;
 
     U3ON = 1; // Serial port enable
-    
+    xprintf(";");
+    manualboot();
+    xprintf("start\n");
     // Z80 start
-    LATA5 = 1; // HALT negate
-    LATE0 = 1; // RESET negate
-    
+    HALT_off();
+    RESET_off();    // RESET negate
+
     // TEST Pin RD7
     ANSELD7 = 0;
     TRISD7 = 0;
 #define TOGGLE do { LATD7 ^= 1; } while(0)
     while(1){
         while(RA1); // Wait for AS = 0
-        ab.h = PORTD; // Read address high
+        ab.h = PORTD & 0x7f; // Read address high
+                             // Ignore A15/RD7 bit(now TEST output pin)
         ab.l = PORTB; // Read address low
     
         if(RA2) { // MC68002 read cycle (RW = 1)
@@ -218,6 +367,9 @@ void main(void) {
             } else { // Out of memory
                 LATC = 0xff; // Invalid data
             }
+            TOGGLE;
+            monitor();
+            TOGGLE;
             while(RA0); // Wait for DS = 0;
             RA4 = 0; // DTACK assert
             while(!RA0); // Wait for DS = 1;
